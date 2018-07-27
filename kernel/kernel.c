fe293ad0079255f88511de0945761dadc465e6c5
danielsmith0612 <danielsmith0612@gmail.com>
2018-07-27 23:36:52 +0900

    Docs : update

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,32 +1,10 @@
-#define VGA_VIDEO_COLUMMS 80
-#define VGA_VIDEO_LINES 25
-#define VGA_VIDEO_CHARACTER_BYTE 2
-#define VGA_VIDIO_MEMORY_ADDRESS 0xb8000 //General-Purpose I/O(256bytes 8000~80FF) in Memory-mapped I/O
+#include "linux/video.h"
 
 void kmain(void)
 {
-        const char *VideoText = "daniel smith kernel";
-        volatile char *VideoMemory = (volatile char *)VGA_VIDIO_MEMORY_ADDRESS;
-		
-		unsigned int VideoTextNumber = 0;
-		unsigned int VideoNumber = 0;
+		char *VideoText = "daniel smith kernel";
 
-		// Initialization VGA Memory space
-	    while(VideoNumber < VGA_VIDEO_COLUMMS * VGA_VIDEO_LINES * VGA_VIDEO_CHARACTER_BYTE) {
-		        VideoMemory[VideoNumber] = ' ';
-				//VGA Text mode color(0x0F is set background to black and text color white)
-		        VideoMemory[VideoNumber + 1] = 0x0F; 		
-		        VideoNumber = VideoNumber + 2;
-	    }
-
-	    VideoNumber = 0;
-
-	    while(VideoText[VideoTextNumber] != '\0') {
-		        VideoMemory[VideoNumber] = VideoText[VideoTextNumber];
-		        VideoMemory[VideoNumber + 1] = 0x0F;
-		        ++VideoTextNumber;
-		        VideoNumber = VideoNumber + 2;
-	    }
-        
+		vga_cls(0x0F);
+		vga_write(VideoText, 0x0F);
 	return;
 }
